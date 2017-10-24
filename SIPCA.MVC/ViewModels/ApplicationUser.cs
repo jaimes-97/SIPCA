bc901378de84f9eef2feed3b0316f689e43155ea
jaimes-97 <solisjaime.97@gmail.com>
2017-10-23 18:35:38 -0600

    ApplicationUser y Config

---
+++ SIPCA.MVC/ViewModels/ApplicationUser.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIPCA.MVC.ViewModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+
+
+        [Display(Name = "Nombre")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Nombre completo")]
+        public string FullName {
+            get { return FirstName + " " + LastName; }
+        }
+
+
+
+        //Relaciones
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+
+    }
+}
---
+++ SIPCA.MVC/ViewModels/ApplicationUserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace SIPCA.MVC.ViewModels
+{
+    public class ApplicationUserConfiguration : EntityTypeConfiguration<ApplicationUser>
+    {
+
+
+        public ApplicationUserConfiguration()
+        {
+
+            Property(au => au.FirstName).HasMaxLength(20).IsRequired();
+            Property(au => au.LastName).HasMaxLength(20).IsOptional();
+            Ignore(au => au.RoleList);
+        }
+    }
+}
